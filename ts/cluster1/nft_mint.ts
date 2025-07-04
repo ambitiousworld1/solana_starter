e2c1bee51c7666aefce49ef26b62491f71595f89
ambitiousworld1 <ambitiousworld@proton.me>
2025-07-04 19:40:51 +0530

    Update nft_mint.ts

--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -2,7 +2,7 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../turbin3-wallet.json"
+import wallet from "../Turbin3-wallet.json"
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -16,11 +16,19 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
+     let tx = createNft(umi, {
+        mint,
+        name: "Penguin",
+        symbol: "PNFT",
+        uri: "https://gateway.irys.xyz/5NW3nU5S9bxEG5QU85bE2YTg7qDhcwdA67WEJuTncy8L",
+        sellerFeeBasisPoints: percentAmount(5), 
+     });
+     let result = await tx.sendAndConfirm(umi);
+     const signature = base58.encode(result.signature);
     
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
-
+     console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+     //Succesfully Minted! Check out your TX here:
+https://explorer.solana.com/tx/512MgeruHzyZZXNnAm5p9WSjayEZEkvC3MsQYq1qaN9Q9mYMAAaYeLyMh5DB58rcNgp9BFjiku8NArod4qWGhHzF?cluster=devnet
     console.log("Mint Address: ", mint.publicKey);
+    //Mint Address:  DiRys2atFKvHcthyAaJGng7WZZ9ksiHRxQbz84RZ1uWR
 })();
